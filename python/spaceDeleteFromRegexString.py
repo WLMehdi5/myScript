a256bea572b82d31de688c916c34b278fa957940
Mehdi ko <wl.mehdi5@gmail.com>
2021-01-21 23:03:28 +0900

    no message

--- python/spaceDeleteFromRegexString.py
+++ python/spaceDeleteFromRegexString.py
@@ -6,8 +6,8 @@ files = glob.glob("./python/**/*.md", recursive=True)
 for file in files:
     print(file + ":start")
     with open(file) as inFile:
+        lines = inFile.readlines()
         with open(file.replace(" ",""),mode="w") as outFile:
-            lines = inFile.readlines()
             for line in lines:
                 if(re.match("\(.*\s.*\)",line)):
                     outFile.write(line.replace(" ",""))
